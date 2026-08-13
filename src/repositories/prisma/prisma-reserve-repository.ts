a29cc293290aea888969fba2c47f4054146acdff
cauealb <cauealvesb4@gmail.com>
2026-08-12 21:25:28 -0300

    feat: Incluindo repository por completo do user

---
+++ src/repositories/prisma/prisma-reserve-repository.ts
@@ -0,0 +1,5 @@
+import type { reserveRepository } from "../reserve-repository.ts";
+
+// export class PrismaReserveRepository implements reserveRepository {
+
+// }
---
+++ src/repositories/prisma/prisma-user-repository.ts
@@ -0,0 +1,19 @@
+import { prisma } from "../../lib/prisma.ts";
+import type { User } from "../../types/user.ts";
+import type { userRepository } from "../user-repository.ts";
+
+export class PrismaUserRepository implements userRepository {
+    async create(data: User) {
+        return await prisma.user.create({
+            data: data
+        })
+    }
+
+    async findById(idUser: string): Promise<User | null> {
+        const user = await prisma.user.findUnique({ where: { idUser: idUser } })
+
+        if(!user) return null
+
+        return user
+    }
+}
