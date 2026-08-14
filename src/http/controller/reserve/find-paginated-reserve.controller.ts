d5e7f1ba4e6ecb0d513a12ae6eb076dcb6566fb1
cauealb <cauealvesb4@gmail.com>
2026-08-13 22:59:29 -0300

    feat: Criando controller de rota que exibe reservas por página

--- src/http/controller/reserve/find-paginated-reserve.controller.ts
+++ src/http/controller/reserve/find-paginated-reserve.controller.ts
@@ -1,3 +1,16 @@
-export async function findPaginatedReserve() {
-    
+import type { FastifyReply, FastifyRequest } from "fastify";
+import z from "zod";
+import { MakeFindPaginatedReserveUseCase } from "../../../module/reserve/factories/make-find-paginated-reserve-use-case.ts";
+
+export async function findPaginatedReserve(request: FastifyRequest, reply: FastifyReply) {
+    const schemaFindPaginatedReserveQuery = z.object({
+        page: z.coerce.number().min(1).default(1)
+    })
+
+    const { page } = schemaFindPaginatedReserveQuery.parse(request.body)
+
+    const useCase = MakeFindPaginatedReserveUseCase()
+    const reserves = await useCase.execute({ page })
+
+    reply.status(200).send(reserves)
 }
--- src/http/controller/reserve/routes.ts
+++ src/http/controller/reserve/routes.ts
@@ -2,10 +2,13 @@ import type { FastifyInstance } from "fastify";
 import { createReserve } from "./create-reserve.controller.ts";
 import { confirmReserve } from "./confirm-reserve.controller.ts";
 import { cancelReserve } from "./cancel-reserve.controller.ts";
+import { findPaginatedReserve } from "./find-paginated-reserve.controller.ts";
 
 export async function reservesRoutes(app: FastifyInstance) {
     app.post('/reserve', createReserve)
 
     app.patch('/confirm/reserve/:idReserve', confirmReserve)
     app.patch('/cancel/reserve/:idReserve', cancelReserve)
+
+    app.get('/reserve/:page', findPaginatedReserve)
 }
