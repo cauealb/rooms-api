dc6919dc3cce4b0b0bcf786969f5e535284d4f8c
cauealb <cauealvesb4@gmail.com>
2026-08-13 22:21:23 -0300

    feat: Incluindo controller de criação de reserva

---
+++ src/http/controller/reserve/create-reserve.controller.ts
@@ -0,0 +1,30 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+import z from "zod";
+import { MakeCreateReserveUseCase } from "../../../module/reserve/factories/make-create-reserve-use-case.ts";
+
+export async function createReserves(request: FastifyRequest, reply: FastifyReply) {
+    const schemaCreateReserveQuery = z.object({
+        idRoom: z.string(),
+        idUser: z.string()
+    })
+
+    const schemaCreateReservesBody = z.object({
+        startOfReserve: z.string(),
+        endOfReserve: z.string(),
+    })
+
+    const { idRoom, idUser } = schemaCreateReserveQuery.parse(request.query)
+    const { startOfReserve, endOfReserve } = schemaCreateReservesBody.parse(request.body)
+
+    const data = {
+        idRoom, 
+        idUser,
+        startOfReserve,
+        endOfReserve
+    }
+
+    const useCase = MakeCreateReserveUseCase()
+    const reserve = await useCase.execute(data)
+
+    reply.status(201).send(reserve)
+}
