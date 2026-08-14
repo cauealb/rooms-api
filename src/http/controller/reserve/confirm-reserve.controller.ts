7d19c60849f90350e6a414ef8ef0335ce9378300
cauealb <cauealvesb4@gmail.com>
2026-08-13 22:50:02 -0300

    feat: Criando controller de confirmação de reserva

---
+++ src/http/controller/reserve/confirm-reserve.controller.ts
@@ -0,0 +1,16 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+import z from "zod";
+import { MakeConfirmReserveUseCase } from "../../../module/reserve/factories/make-confirm-reserve-use-case.ts";
+
+export async function confirmReserve(request: FastifyRequest, reply: FastifyReply) {
+    const schemaConfirmReserveQuery = z.object({
+        idReserve: z.string()
+    })
+
+    const { idReserve } = schemaConfirmReserveQuery.parse(request.body)
+
+    const useCase = MakeConfirmReserveUseCase()
+    const reserve = await useCase.execute({ idReserve })
+
+    reply.status(200).send(reserve)
+}
