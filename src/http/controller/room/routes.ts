9144361fb295109a4d1cd8ba36899ac18f2ac88f
cauealb <cauealvesb4@gmail.com>
2026-08-17 22:13:27 -0300

    feat: criando hook para rotas que precisam do access token

--- src/http/controller/room/routes.ts
+++ src/http/controller/room/routes.ts
@@ -1,6 +1,7 @@
 import type { FastifyInstance } from "fastify";
 import { createRoom } from "./create-room.controller.ts";
+import { verifyToken } from "../../../middlewares/verify-token.ts";
 
 export async function roomRoutes(app: FastifyInstance) {
-    app.post('/room', createRoom)
+    app.post('/room', {onRequest: [verifyToken]}, createRoom)
 }
--- src/middlewares/verify-token.ts
+++ src/middlewares/verify-token.ts
@@ -3,8 +3,11 @@ import { SessionExpiredError } from "../errors/session-expired-error.ts";
 
 export async function verifyToken(request: FastifyRequest, reply: FastifyReply) {
     try {
+        console.log('olaaaaaaa')
         await request.jwtVerify()
     } catch (ex) {
-        throw new SessionExpiredError
+        return reply.status(401).send({
+            message: 'Unauthorized'
+        })
     }
 }
