9db558271f8d3bac9a8d79554210a18cf7bed581
cauealb <cauealvesb4@gmail.com>
2026-08-21 00:10:07 -0300

    db: Mudando schema do banco de dados para mais colunas de usuário

---
+++ src/module/User/use-cases/authenticate-user-use-case.ts
@@ -0,0 +1,34 @@
+import type { User } from "../../../generated/prisma/client.ts";
+import type { userRepository } from "../../../repositories/user-repository.ts";
+import { compare } from 'bcrypt'
+
+interface AutheticateUserUseCaseRequest {
+    email: string
+    password: string
+}
+
+interface AutheticateUserUseCaseResponse {
+    user: User
+}
+
+export class AutheticateUserUseCase {
+    private readonly userRepository: userRepository
+
+    constructor(repository: userRepository) {
+        this.userRepository = repository
+    }
+
+    async execute({ email, password }: AutheticateUserUseCaseRequest): Promise<AutheticateUserUseCaseResponse> {
+        const user = await this.userRepository.findByEmail(email)
+        if(!user) {
+            throw new Error()
+        }
+
+        const isPasswordMatches = await compare(user.password, password)
+        if(!isPasswordMatches) {
+            throw new Error()
+        }
+
+        return { user }
+    }
+}
--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -2,6 +2,8 @@ import type { User, UserCreate } from "../types/user.ts";
 
 export interface userRepository {
     create(data: UserCreate): Promise<User>
+
+    findByEmail(email: string): Promise<User | null>
     findById(idUser: string): Promise<User | null>
 }
 
